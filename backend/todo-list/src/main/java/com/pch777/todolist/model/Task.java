df1fbdf9f4fcf0af3ca2847f59b3b42fd22a3f49
Pawel Chmiel <pch777@gazeta.pl>
2020-05-25 12:10:48 +0200

    create class Task

---
+++ backend/todo-list/src/main/java/com/pch777/todolist/model/Task.java
@@ -0,0 +1,53 @@
+package com.pch777.todolist.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	
+	private LocalDate createOn;
+
+	public Task() {
+	}
+
+	public Task(String title, LocalDate createOn) {
+		this.title = title;
+		this.createOn = createOn;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getCreateOn() {
+		return createOn;
+	}
+
+	public void setCreateOn(LocalDate createOn) {
+		this.createOn = createOn;
+	}
+
+}
